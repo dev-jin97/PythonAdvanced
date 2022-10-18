5105ae32e591c439df17357a2b2c831028147d1c
Hyeonjin Kim <guswls0975@naver.com>
2022-10-18 13:46:08 +0900

    add: 09. Various_method learning and simple practice

---
+++ Chapter4/09.inheritance.py
@@ -0,0 +1,48 @@
+
+class Item:
+    """
+    Attr: name,
+    Method: drow, drop
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def pick(self):
+        print(f"[{self.name}]을(를) 주웠습니다.")
+
+    def drop(self):
+        print(f"[{self.name}]을(를) 버렸습니다.")
+
+class Weapon(Item):
+    """
+    Attr: demage
+    metho: attack
+    """
+    def __init__(self, name, demage):
+        super().__init__(name)
+        self.demage = demage
+
+    def attack(self):
+        print(f"[{self.name}]을(를) 이용해 {self.demage}로 공격합니다.")
+
+
+class HealingItem(Item):
+    """
+    Attr: recovery_aomount
+    method: use
+    """
+    def __init__(self, name, recovery_amount):
+        super().__init__(name)
+        self.recovery_amount = recovery_amount
+
+    def use(self):
+        print(f"[{self.name}]을(를) 사용합니다. {self.recovery_amount} 회복")
+
+
+m16 = Weapon("m16", 110)
+band = HealingItem("붕대", 20)
+
+m16.attack()
+band.use()
+
---
+++ Chapter4/09.inheritance_2.py
@@ -0,0 +1,54 @@
+from abc import *
+
+class Item(metaclass=ABCMeta):
+    """
+    Attr: name,
+    Method: drow, drop
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def pick(self):
+        print(f"[{self.name}]을(를) 주웠습니다.")
+
+    def drop(self):
+        print(f"[{self.name}]을(를) 버렸습니다.")
+
+    @abstractmethod
+    def use(self):
+        pass
+
+
+class Weapon(Item):
+    """
+    Attr: demage
+    metho: attack
+    """
+    def __init__(self, name, demage):
+        super().__init__(name)
+        self.demage = demage
+
+    def use(self):
+        print(f"[{self.name}]을(를) 이용해 {self.demage}로 공격합니다.")
+
+
+class HealingItem(Item):
+    """
+    Attr: recovery_aomount
+    method: use
+    """
+    def __init__(self, name, recovery_amount):
+        super().__init__(name)
+        self.recovery_amount = recovery_amount
+
+    def use(self):
+        print(f"[{self.name}]을(를) 사용합니다. {self.recovery_amount} 회복")
+
+
+m16 = Weapon("m16", 110)
+band = HealingItem("붕대", 20)
+
+m16.use()
+band.use()
+
